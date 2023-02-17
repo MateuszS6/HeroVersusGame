05ff41532ba0eaa5e2157f0e4dddb958f9365411
MateuszStepien1104 <mateusz.stepien@city.ac.uk>
2023-02-17 13:04:27 +0000

    background image added

--- src/game/GameView.java
+++ src/game/GameView.java
@@ -2,8 +2,18 @@ package game;
 
 import city.cs.engine.UserView;
 
+import javax.swing.*;
+
+import java.awt.*;
+
 public class GameView extends UserView {
+    private final Image background = new ImageIcon("data/university.gif").getImage();
     public GameView(GameWorld world, int width, int height) {
         super(world, width, height);
+    }
+
+    @Override
+    protected void paintBackground(Graphics2D g) {
+        g.drawImage(background, 0, 0, this);
     }
 }
