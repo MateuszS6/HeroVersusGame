3da5d543d0e918cd9c262d8fa6da28b9eb33c7e3
MateuszStepien1104 <mateusz.stepien@city.ac.uk>
2023-03-10 17:25:03 +0000

    Added platform class and collision listener

---
+++ src/game/Platform.java
@@ -0,0 +1,11 @@
+package game;
+
+import city.cs.engine.Shape;
+import city.cs.engine.StaticBody;
+import city.cs.engine.World;
+
+public class Platform extends StaticBody {
+    public Platform(World world, Shape shape) {
+        super(world, shape);
+    }
+}
