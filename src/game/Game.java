1e8cae44cea5136590eddac4c81267f797d066ee
MateuszStepien1104 <mateusz.stepien@city.ac.uk>
2023-04-30 17:28:02 +0100

    Added some GUI assets

--- src/game/Game.java
+++ src/game/Game.java
@@ -10,9 +10,12 @@ public final class Game {
     private BattleArena arena;
     private GameView view;
     public static final float GRID = 1;
+    private JFrame window;
 
     /** Initialise a new game. */
     public Game() {
+        // TODO: 30/04/2023 Main menu book-style as imagined
+
         arena = new Royal(); // Game world
 
         view = new GameView(arena, 800, 600); // Game view
@@ -30,13 +33,13 @@ public final class Game {
     }
 
     private void buildWindow() {
-        final JFrame frame = new JFrame("HeroVersus: Battle Arena");
-        frame.add(view); // Add the game view to the frame
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationByPlatform(true);
-        frame.setResizable(false);
-        frame.pack(); // Resize the frame to fit world view
-        frame.setVisible(true);
+        window = new JFrame("HeroVersus: Battle Arena");
+        window.add(view); // Add the game view to the window
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationByPlatform(true);
+        window.setResizable(false);
+        window.pack(); // Resize the window to fit world view
+        window.setVisible(true);
     }
 
     public void debugTools(boolean grid, int res, boolean viewer) {
@@ -44,13 +47,14 @@ public final class Game {
         if (viewer) new DebugViewer(arena, view.getWidth(), view.getHeight());
     }
 
-    // TODO: 29/04/2023 Winning player of final round is CHAMPION
+    // TODO: 29/04/2023 Winning player of final round is ULTIMATE CHAMPION
     public void goToArena(BattleArena w) {
         arena.stop();
         arena = w;
         view.setWorld(arena);
         // TODO: 29/04/2023 Update player controllers
         arena.start();
+        
     }
 
     public void restart() {
